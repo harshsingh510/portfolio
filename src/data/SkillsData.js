ce7c8bc5b0e4b150d519179a4d11b31977e63668
Harsh Singh <harsh510official@gmail.com>
2023-12-10 23:17:41 +0530

    adding to Github

---
+++ src/data/SkillsData.js
@@ -0,0 +1,128 @@
+import {
+	FaBootstrap,
+	FaCss3Alt,
+	FaGithub,
+	FaHtml5,
+	FaNpm,
+	FaReact,
+	// FaSass,
+	FaYarn,
+	FaFigma,
+	FaPhp,
+	FaNodeJs,
+	FaMdb,
+	FaPython
+	
+} from "react-icons/fa";
+import { DiJavascript1 } from "react-icons/di";
+import { VscJson } from "react-icons/vsc";
+import {
+	SiAdobephotoshop,
+	SiTailwindcss,
+	SiNextdotjs,
+	SiTypescript,
+	SiFirebase,
+	SiStyledcomponents,
+	SiFramer,
+	// SiMaterialui,
+	SiRedux,
+	SiMongodb,
+	SiExpress,
+	SiCplusplus,
+	SiJava
+} from "react-icons/si";
+
+export const SkillsData = [
+	{
+		name: "HTML",
+		icon: <FaHtml5 />,
+	},
+	{
+		name: "CSS",
+		icon: <FaCss3Alt />,
+	},
+	{
+		name: "Javascript",
+		icon: <DiJavascript1 />,
+	},
+	{
+		name: "TypeScript",
+		icon: <SiTypescript />,
+	},
+	{
+		name: "React Js",
+		icon: <FaReact />,
+	},
+	{
+		name: "Next Js",
+		icon: <SiNextdotjs />,
+	},
+	{
+		name: "Redux",
+		icon: <SiRedux />,
+	},
+	{
+		name: "PHP",
+		icon: < FaPhp/>,
+	},
+	{
+		name: "JSON",
+		icon: <VscJson />,
+	},
+	{
+		name: "Bootstrap",
+		icon: <FaBootstrap />,
+	},
+	{
+		name: "Tailwind",
+		icon: <SiTailwindcss />,
+	},
+	{
+		name: "NodeJs",
+		icon: <FaNodeJs />,
+	},
+	{
+		name: "MongoDB",
+		icon: <SiMongodb />,
+	},
+	{
+		name: "Styled Components",
+		icon: <SiStyledcomponents />,
+	},
+	{
+		name: "Framer Motion",
+		icon: <SiFramer />,
+	},
+	{
+		name: "Git",
+		icon: <FaGithub />,
+	},
+	{
+		name: "Npm",
+		icon: <FaNpm />,
+	},
+	{
+		name: "Yarn",
+		icon: <FaYarn />,
+	},
+	{
+		name: "Figma",
+		icon: <FaFigma />,
+	},
+	{
+		name: "Express Js",
+		icon: <SiExpress />,
+	},
+	{
+		name: "Python",
+		icon: <FaPython />,
+	},
+	{
+		name: "C++",
+		icon: <SiCplusplus />,
+	},
+	{
+		name: "Java",
+		icon: <SiJava />,
+	},
+];
---
+++ src/screen/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from "react"
+import { BrowserRouter as Router } from "react-router-dom";
+import '../styles/App.css';
+import { motion } from "framer-motion";
+import HomePage from "../pages/HomePage";
+import { FaAlignCenter } from "react-icons/fa";
+import { BsAlignCenter } from "react-icons/bs";
+
+function App() {
+  // States
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    setTimeout(() => setLoading(false), 3000)
+  }, []);
+
+  const loadText = {
+    hidden: {
+      opacity: 0,
+    },
+    visible: {
+      opacity: 1,
+      transition:{
+        duration: 1
+      }
+    }
+  }
+
+
+  return (
+    loading ? 
+    <div className="loader">
+      <div className="svg-wrapper">
+        <svg  xmlns="http://www.w3.org/2000/svg" >
+          <rect className="shape"  />
+        </svg>        
+        <motion.p variants={loadText} initial='hidden' animate='visible' className="text">Harsh Singh</motion.p>
+      </div>
+    </div>
+    :
+    <div className="App">
+      <Router>
+        <HomePage />
+      </Router>
+    </div>
+  );
+}
+
+export default App;
