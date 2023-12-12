e9f2e5177dd4491e8dd96713b293c685aa8a47e1
Harsh Singh <harsh510official@gmail.com>
2023-12-12 23:52:45 +0530

    Links updated

--- src/components/navbar/NavLinks.js
+++ src/components/navbar/NavLinks.js
@@ -8,8 +8,8 @@ const NavLinks = ({handleNav}) => {
   return (
     <ul className='nav-links'>
         <li onClick={handleNav} ><Link  to="//twitter.com/thakur_shahhb" target='_blank' className='nav-link'><FaTwitter /></Link></li>
-        <li onClick={handleNav} ><Link  to="https://www.linkedin.com/in/harsh-singh-90b887214/" target='_blank' className='nav-link'><FaLinkedin /></Link></li>
-        <li onClick={handleNav} ><Link  to="https://github.com/harshsingh510" target='_blank' className='nav-link'><BsGithub /></Link></li>
+        <li onClick={handleNav} ><Link  to="//www.linkedin.com/in/harsh-singh-90b887214/" target='_blank' className='nav-link'><FaLinkedin /></Link></li>
+        <li onClick={handleNav} ><Link  to="//github.com/harshsingh510" target='_blank' className='nav-link'><BsGithub /></Link></li>
         {/* <li onClick={handleNav} ><Link  to="//www.instagram.com/Tthakur_shahhb" target='_blank' className='nav-link'><FaInstagram /></Link></li> */}
         <li onClick={handleNav} ><Link  to="//mailto:harsh510official@gmail.com" target='_blank' className='nav-link'><BiEnvelope /></Link></li>
   </ul>
